3be9aa5bbf156ceb5ee7d867adf07663bf7910e3
hshi74 <haochen213300@gmail.com>
2024-03-31 15:14:17 -0700

    Update the toddlerbot config

--- legged_gym/envs/toddlerbot/toddlerbot_config.py
+++ legged_gym/envs/toddlerbot/toddlerbot_config.py
@@ -4,14 +4,15 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 class ToddlerbotCfg(LeggedRobotCfg):
     class env(LeggedRobotCfg.env):
         num_envs = 4096
-        num_observations = 253
+        num_observations = 66
         num_actions = 18
 
     class terrain(LeggedRobotCfg.terrain):
         mesh_type = "plane"
+        measure_heights = False
 
     class init_state(LeggedRobotCfg.init_state):
-        pos = [0.0, 0.0, 1.0]  # x,y,z [m]
+        pos = [0.0, 0.0, 0.0]  # x,y,z [m]
         default_joint_angles = {  # = target angles [rad] when action = 0.0
             "left_hip_yaw": 0.0,
             "left_hip_roll": 0.0,
@@ -35,7 +36,7 @@ class ToddlerbotCfg(LeggedRobotCfg):
 
     class control(LeggedRobotCfg.control):
         # PD Drive parameters:
-        stiffness = {   
+        stiffness = {
             "left_hip_yaw": 100.0,
             "left_hip_roll": 100.0,
             "left_hip_pitch": 100.0,
@@ -55,25 +56,25 @@ class ToddlerbotCfg(LeggedRobotCfg):
             "right_sho_roll": 100.0,
             "right_elb": 100.0,
         }  # [N*m/rad]
-        damping = { 
-            "left_hip_yaw": 10.0,
-            "left_hip_roll": 10.0,
-            "left_hip_pitch": 10.0,
-            "left_knee": 10.0,
-            "left_ank_pitch": 10.0,
-            "left_ank_roll": 10.0,
-            "right_hip_yaw": 10.0,
-            "right_hip_roll": 10.0,
-            "right_hip_pitch": 10.0,
-            "right_knee": 10.0,
-            "right_ank_pitch": 10.0,
-            "right_ank_roll": 10.0,
-            "left_sho_pitch": 10.0,
-            "left_sho_roll": 10.0,
-            "left_elb": 10.0,
-            "right_sho_pitch": 10.0,
-            "right_sho_roll": 10.0,
-            "right_elb": 10.0,
+        damping = {
+            "left_hip_yaw": 4.0,
+            "left_hip_roll": 4.0,
+            "left_hip_pitch": 4.0,
+            "left_knee": 4.0,
+            "left_ank_pitch": 4.0,
+            "left_ank_roll": 4.0,
+            "right_hip_yaw": 4.0,
+            "right_hip_roll": 4.0,
+            "right_hip_pitch": 4.0,
+            "right_knee": 4.0,
+            "right_ank_pitch": 4.0,
+            "right_ank_roll": 4.0,
+            "left_sho_pitch": 4.0,
+            "left_sho_roll": 4.0,
+            "left_elb": 4.0,
+            "right_sho_pitch": 4.0,
+            "right_sho_roll": 4.0,
+            "right_elb": 4.0,
         }  # [N*m*s/rad]
         # action scale: target angle = actionScale * action + defaultAngle
         action_scale = 0.5
@@ -84,7 +85,21 @@ class ToddlerbotCfg(LeggedRobotCfg):
         file = "{LEGGED_GYM_ROOT_DIR}/../../robot_descriptions/toddlerbot/toddlerbot_isaac.urdf"
         name = "toddlerbot"
         foot_name = "ank_roll_link"
-        terminate_after_contacts_on = ["body_link"]
+        terminate_after_contacts_on = [
+            "body_link",
+            "neck_link",
+            "head_link",
+            "hip_roll_link",
+            "left_hip_pitch_link",
+            "left_calf_link",
+            "hip_roll_link_2",
+            "right_hip_pitch_link",
+            "right_calf_link",
+            "sho_roll_link",
+            "elb_link",
+            "sho_roll_link_2",
+            "elb_link_2",
+        ]
         flip_visual_attachments = False
         self_collisions = 1  # 1 to disable, 0 to enable...bitwise filter
 
@@ -108,12 +123,17 @@ class ToddlerbotCfg(LeggedRobotCfg):
             ang_vel_xy = -0.0
             feet_contact_forces = -0.0
 
+    class viewer:
+        pos = [-1, -0.5, 0.5]  # [m]
+        lookat = [0, 0, 0.3]  # [m]
+
 
 class ToddlerbotCfgPPO(LeggedRobotCfgPPO):
 
     class runner(LeggedRobotCfgPPO.runner):
         run_name = ""
         experiment_name = "walk_toddlerbot"
+        max_iterations = 1500
 
     class algorithm(LeggedRobotCfgPPO.algorithm):
         entropy_coef = 0.01
